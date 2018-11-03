4f78209166936ff9a75d833056afd6a3a7591641
Anubhav Ashish <anubhav.ashish9@gmail.com>
2018-11-03 15:52:15 +1100

    commiting

--- IntroTodNode/YelpCamp/app.js
+++ IntroTodNode/YelpCamp/app.js
@@ -1,24 +1,34 @@
-var express = require("express");
-var app = express();
-var request = require("request");
-var accessKey = "f3ba25906107038aa1b834248ff6185615dabb09a6f5dd6757a5b8ea04c70678";
-var secretKey = "075caf8e066853292948546c944dfca83814784c789538ff3c291688b84ada91";
+var express = require("express"),
+    app     = express(),
+    request = require("request"),
+    mongoose = require("mongoose");
+
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
-var campGrounds = [
-    {name: "YellowStone National Park", url: "https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg"},
-    {name: "MilkyWay National Park", url: "https://cdn.pixabay.com/photo/2015/09/01/06/17/milky-way-916523_960_720.jpg"},
-    {name: "Andromeda National Park", url: "https://cdn.pixabay.com/photo/2015/05/06/16/31/andromeda-galaxy-755442_960_720.jpg"},
-    {name: "YellowStone National Park", url: "https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg"},
-    {name: "MilkyWay National Park", url: "https://cdn.pixabay.com/photo/2015/09/01/06/17/milky-way-916523_960_720.jpg"},
-    {name: "Andromeda National Park", url: "https://cdn.pixabay.com/photo/2015/05/06/16/31/andromeda-galaxy-755442_960_720.jpg"},
-    {name: "YellowStone National Park", url: "https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg"},
-    {name: "MilkyWay National Park", url: "https://cdn.pixabay.com/photo/2015/09/01/06/17/milky-way-916523_960_720.jpg"},
-    {name: "Andromeda National Park", url: "https://cdn.pixabay.com/photo/2015/05/06/16/31/andromeda-galaxy-755442_960_720.jpg"},
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
+var Campground = mongoose.model("campground", campgroundSchema);
 
-];
+Campground.create(
+    {
+        name: "YellowStone National Park",
+        image: "https://www.nps.gov/npgallery/GetAsset/ABE1C874-155D-451F-678E2B7A1B7D2229/proxy/hires/",
+        description: "This is an yellowstone park. It is very beautiful!"
+    },
+    function(error, camp){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(camp);
+        }
+});
 
 var PORT = "3000";
 var IP = "127.0.0.1";
@@ -32,17 +42,32 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX route 
 //CampGrounds 
 app.get("/campgrounds", function(req, res){
-        res.render("campgrounds", {campGrounds: campGrounds});
+    Campground.find({}, function(error, campGrounds){
+        if(error){
+            console.log(error);
+        }else{
+            res.render("index", {campGrounds: campGrounds});
+        }
+    });
+    
 });
 
-//Create new campgrounds 
+//Create Route 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var imageURL = req.body.imageURL;
+    var desc = req.body.description;
     console.log(name +" "+ imageURL);
-    campGrounds.push({name: name, url : imageURL});
+    Campground.create({name: name, image: imageURL, description: desc}, function(error, camp){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(camp);
+        }
+    })
     res.redirect("/campgrounds");
 });
 
@@ -50,3 +75,13 @@ app.post("/campgrounds", function(req, res){
 app.get("/campgrounds/new", function(req, res){
         res.render("new");
 });
+
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log("Error");
+        }else{
+            res.render("show",{campGround :foundCampground});
+        }
+    });
+})
