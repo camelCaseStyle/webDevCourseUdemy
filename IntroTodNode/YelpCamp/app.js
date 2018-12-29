39712ba453ec5f6606e4089a45d967912005a03a
Anubhav Ashish <anubhav.ashish9@gmail.com>
2018-12-29 18:44:13 +1100

    updates with new files

--- IntroTodNode/YelpCamp/app.js
+++ IntroTodNode/YelpCamp/app.js
@@ -1,34 +1,50 @@
 var express = require("express"),
     app     = express(),
     request = require("request"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds"),
+    methodOverride = require("method-override"),
+    Comment = require("./models/comment"),
+    flash = require("connect-flash");
 
+var campgroundRoutes = require("./routes/campgrounds"),
+    commentRoutes = require("./routes/comments"),
+    indexRoutes = require("./routes/index");
+
+//seedDB();    
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(flash());
+app.use(express.static(__dirname+"/public"));
+app.use(methodOverride("_method"));
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-})
-var Campground = mongoose.model("campground", campgroundSchema);
-
-Campground.create(
-    {
-        name: "YellowStone National Park",
-        image: "https://www.nps.gov/npgallery/GetAsset/ABE1C874-155D-451F-678E2B7A1B7D2229/proxy/hires/",
-        description: "This is an yellowstone park. It is very beautiful!"
-    },
-    function(error, camp){
-        if(error){
-            console.log(error);
-        }else{
-            console.log(camp);
-        }
+//PASSPORT CONFIG 
+app.use(require("express-session")({
+    secret: "james",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next){
+    res.locals.user = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    res.locals.info = req.flash("info");
+    next();
 });
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 var PORT = "3000";
 var IP = "127.0.0.1";
@@ -37,51 +53,7 @@ app.listen(PORT, IP, function(){
     console.log("The server is up!");
 });
 
-//Landing page 
-app.get("/", function(req, res){
-    res.render("landing");
-});
+app.use(indexRoutes);
+app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id",commentRoutes);
 
-//INDEX route 
-//CampGrounds 
-app.get("/campgrounds", function(req, res){
-    Campground.find({}, function(error, campGrounds){
-        if(error){
-            console.log(error);
-        }else{
-            res.render("index", {campGrounds: campGrounds});
-        }
-    });
-    
-});
-
-//Create Route 
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var imageURL = req.body.imageURL;
-    var desc = req.body.description;
-    console.log(name +" "+ imageURL);
-    Campground.create({name: name, image: imageURL, description: desc}, function(error, camp){
-        if(error){
-            console.log(error);
-        }else{
-            console.log(camp);
-        }
-    })
-    res.redirect("/campgrounds");
-});
-
-//Show the form which will show the data to send to this form 
-app.get("/campgrounds/new", function(req, res){
-        res.render("new");
-});
-
-app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            console.log("Error");
-        }else{
-            res.render("show",{campGround :foundCampground});
-        }
-    });
-})
